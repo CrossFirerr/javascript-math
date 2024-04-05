f8d62454c3f23b70c94552c49daebdbee86bf800
CrossFirerr <156629661+CrossFirerr@users.noreply.github.com>
2024-04-05 15:54:02 +1100

    compltete math challange

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
 ////create random number///
-const arg = [];
-for (let i = 0; i < 50; i++) {
-  const num = Math.floor(Math.random() * 100 + 1);
-  arg.push(num);
-}
+// const arg = [];
+// for (let i = 0; i < 50; i++) {
+//   const num = Math.floor(Math.random() * 100 + 1);
+//   arg[i] = num;
+// }
 
-///sort arry in decending order///
+// ///sort arry in decending order///
 
-arg.sort((a, b) => {
-  b - a;
-});
+// arg.sort((a, b) => {
+//   b - a;
+// });
 // console.log(arg);
-///total of the array ////
-const total = arg.reduce((acc, num) => acc + num, 0);
-// console.log(total);
+// ///total of the array ////
+// const total = arg.reduce((acc, num) => acc + num, 0);
+// // console.log(total);
 
-///divide into 2 new array////
+// ///divide into 2 new array////
 
-const evenArg = arg.filter((item) => item % 2 === 0);
-const oddArg = arg.filter((item) => item % 2 !== 0);
-console.log(evenArg, oddArg);
+// const evenArg = arg.filter((item) => item % 2 === 0);
+// const oddArg = arg.filter((item) => item % 2 !== 0);
+// console.log(evenArg, oddArg);
 
-///remove duplicare item///
-// const uniqArg = [...new Set(arg)];
+// ///remove duplicare item///
+// // const uniqArg = [...new Set(arg)];
+// const uniqArg = [];
+// for (let i = 0; i < arg.length; i++) {
+//   ///only uniq data
+//   if (!uniqArg.includes(arg[i])) {
+//     uniqArg.push(arg[i]);
+//   }
+// }
+// console.log(uniqArg);
+
 const uniqArg = [];
-for (let i = 0; i < arg.length; i++) {
-  ///only uniq data
-  if (!uniqArg.includes(arg[i])) {
-    uniqArg.push(arg[i]);
+for (let i = 0; uniqArg.length < 50; i++) {
+  const num = Math.floor(Math.random() * 100) + 1;
+  if (!uniqArg.includes(num)) {
+    uniqArg.push(num);
   }
+  console.log(i);
 }
 console.log(uniqArg);
