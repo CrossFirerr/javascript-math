9e6863791004b16bf621748c31cc5d5387162af5
CrossFirerr <156629661+CrossFirerr@users.noreply.github.com>
2024-04-05 21:29:23 +1100

    date check

--- script.js
+++ script.js
@@ -1,43 +1,70 @@
-////create random number///
-// const arg = [];
-// for (let i = 0; i < 50; i++) {
-//   const num = Math.floor(Math.random() * 100 + 1);
-//   arg[i] = num;
-// }
+// ////create random number///
+// // const arg = [];
+// // for (let i = 0; i < 50; i++) {
+// //   const num = Math.floor(Math.random() * 100 + 1);
+// //   arg[i] = num;
+// // }
 
-// ///sort arry in decending order///
+// // ///sort arry in decending order///
 
-// arg.sort((a, b) => {
-//   b - a;
-// });
-// console.log(arg);
-// ///total of the array ////
-// const total = arg.reduce((acc, num) => acc + num, 0);
-// // console.log(total);
+// // arg.sort((a, b) => {
+// //   b - a;
+// // });
+// // console.log(arg);
+// // ///total of the array ////
+// // const total = arg.reduce((acc, num) => acc + num, 0);
+// // // console.log(total);
 
-// ///divide into 2 new array////
+// // ///divide into 2 new array////
 
 // const evenArg = arg.filter((item) => item % 2 === 0);
 // const oddArg = arg.filter((item) => item % 2 !== 0);
 // console.log(evenArg, oddArg);
 
-// ///remove duplicare item///
-// // const uniqArg = [...new Set(arg)];
+// // ///remove duplicare item///
+// // // const uniqArg = [...new Set(arg)];
+// // const uniqArg = [];
+// // for (let i = 0; i < arg.length; i++) {
+// //   ///only uniq data
+// //   if (!uniqArg.includes(arg[i])) {
+// //     uniqArg.push(arg[i]);
+// //   }
+// // }
+// // console.log(uniqArg);
+
 // const uniqArg = [];
-// for (let i = 0; i < arg.length; i++) {
-//   ///only uniq data
-//   if (!uniqArg.includes(arg[i])) {
-//     uniqArg.push(arg[i]);
+// for (let i = 0; uniqArg.length < 50; i++) {
+//   const num = Math.floor(Math.random() * 100) + 1;
+//   if (!uniqArg.includes(num)) {
+//     uniqArg.push(num);
 //   }
+//   console.log(i);
 // }
 // console.log(uniqArg);
 
-const uniqArg = [];
-for (let i = 0; uniqArg.length < 50; i++) {
-  const num = Math.floor(Math.random() * 100) + 1;
-  if (!uniqArg.includes(num)) {
-    uniqArg.push(num);
+////// date challange////
+const miliSecPerDay = 24 * 60 * 60 * 1000;
+const dateExp = ({ exp, name }) => {
+  const today = Date.now();
+  const expTS = new Date(exp).getTime();
+  const diffDay = Math.floor((expTS - today) / miliSecPerDay);
+  if (diffDay < 1) {
+    return `${name} expired ${Math.abs(diffDay)} days mate`;
+  } else {
+    return ` ${diffDay} days use it asap ${name}`;
   }
-  console.log(i);
-}
-console.log(uniqArg);
+};
+const foods = [
+  {
+    name: "momo",
+    exp: "2024-3-29",
+  },
+  {
+    name: "milk",
+    exp: "2024-4-7",
+  },
+];
+foods.map((food) => {
+  const result = dateExp(food);
+  console.log(result);
+});
